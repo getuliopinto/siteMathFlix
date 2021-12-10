0a59af9e8fd0b67a8a5ccbb6720082bd12ef1741
getulio pinto <getuliopinto87@gmail.com>
2021-12-10 13:27:43 -0300

    botao Math Calculator funcionando corretamente

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+//faznedo botão 'Math Calculator' funcionar, mesmo estando dentro de uma tag form
+
+$(function () {
+  $("#btn-wolfram").on("click", function(evento){ evento.preventDefault();})
+});
+
